dfc2d74eb89eaf0297e4f6331ebba97ab06d398c
JanusU <janusudsen@yahoo.com>
2017-02-14 13:46:04 +0100

    client updated with thread

--- ChatClient/src/main/java/Client.java
+++ ChatClient/src/main/java/Client.java
@@ -1,20 +1,64 @@
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Janus
  */
 public class Client {
-    Scanner sc = new Scanner(System.in);
-    
-    public static void main(String[] args) {
-        
+
+    private final String host = "localhost";
+    private final int port = 8080;
+    private Socket clientSocket;
+    public Boolean connected = false;
+
+    public static void main(String[] args) throws IOException {
+        Client cl = new Client();
+        Scanner sc = new Scanner(System.in);
+        String input;
+        while (sc.hasNext()) {
+            input = (String) sc.next();
+
+            switch (input) {
+                case "login":
+                    System.out.println("Skriv dit username:");
+                    cl.login((String) sc.next());
+                    break;
+
+                default:
+                    if (cl.connected) {
+                        cl.sendMessage("MSG#" + input);
+                    } else {
+                        System.out.println("Please login to the server by typing: login");
+                    }
+                    break;
+            }
+        }
     }
+
+    public void login(String username) throws IOException {
+        //connects to the server with username
+        clientSocket = new Socket();
+        clientSocket.connect(new InetSocketAddress(host, port));
+        connected = true;
+        System.out.println("Connecting to chatserver");
+        sendMessage("LOGIN#" + username);
+    }
+
+    public void sendMessage(String message) throws IOException {
+        // Creates new Thread and writes to the server
+        new Thread(new MsgCon(clientSocket.getOutputStream(),message)).start();
+    }
+    
 }
---
+++ ChatClient/src/main/java/MsgCon.java
@@ -0,0 +1,31 @@
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.function.Consumer;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Janus
+ */
+public class MsgCon implements Runnable{
+    private OutputStream output;
+    private String message;
+    public MsgCon(OutputStream out, String msg) {
+        output = out;
+        message = msg;
+        
+    }
+    @Override
+    public void run() {
+        PrintWriter writer = new PrintWriter(output);
+                writer.println(message);
+                writer.flush();
+    }
+}
