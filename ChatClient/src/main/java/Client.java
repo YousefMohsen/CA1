01109fda721e0085a68ea2d2720d1fd04a63e958
JanusU <janusudsen@yahoo.com>
2017-02-14 11:43:09 +0100

    chatclient

---
+++ ChatClient/src/main/java/Client.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Janus
+ */
+public class Client {
+    Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+    }
+}
